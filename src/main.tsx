172827bfd70be1e3cf3afdfad815db2fcdaf9f49
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-26 00:26:55 +0300

    absolute import changes 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "src/App.tsx";
 // Material UI uses the Roboto font by default: https://mui.com/material-ui/getting-started/installation/#roboto-font
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
