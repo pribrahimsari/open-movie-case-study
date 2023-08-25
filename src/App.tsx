817b12a44020aa31fd6e11cda78d2d55175c2b49
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-25 23:48:47 +0300

    new App.tsx: defines browser routes, handles routing, cssBaseline, removes App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import MoviePage from "./pages/MoviePage.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/movie/:imdbId",
+    element: <MoviePage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+// import "./index.css"; // todo: remove
 // Material UI uses the Roboto font by default: https://mui.com/material-ui/getting-started/installation/#roboto-font
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
