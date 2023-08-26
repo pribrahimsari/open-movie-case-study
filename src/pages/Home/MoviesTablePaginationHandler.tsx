3c537ac9cec07bffdab39f9a95061dc3f4e44684
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-26 16:19:48 +0300

    add page info to the footer of table via redux

--- src/pages/Home/MoviesTablePaginationHandler.tsx
+++ src/pages/Home/MoviesTablePaginationHandler.tsx
@@ -1,10 +1,17 @@
-import { Box, IconButton, Tooltip } from "@mui/material";
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { nextPage, previousPage } from "src/redux/SearchVariablesStore/searchVariablesSlice.ts";
+import { useSearchVariablesDispatch, useSearchVariablesSelector } from "src/redux/SearchVariablesStore/SearchVariablesStore.ts";
 
 const MoviesTablePaginationHandler = () => {
+  const page = useSearchVariablesSelector((state) => state.searchVariables.page);
+  const dispatch = useSearchVariablesDispatch();
+
   return (
-    <Box display="flex" justifyContent="flex-end" gap={1}>
+    <Box display="flex" justifyContent="flex-end" alignItems="center" gap={1}>
+      <Typography variant="body2">Page: {page}</Typography>
+
       <Tooltip title={"Previous"}>
         <IconButton size="small" aria-label="Previous">
           <ArrowBackIosIcon />
